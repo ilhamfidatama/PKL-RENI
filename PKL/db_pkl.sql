83c7cca5de499ac7c00941cfbb62ca420f62eea5
ilhamfidatama <ilhamfidatama@gmail.com>
2019-04-26 15:12:23 +0800

    Update db_pkl.sql

--- PKL/db_pkl.sql
+++ PKL/db_pkl.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.7
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 22, 2019 at 05:19 AM
--- Server version: 10.1.30-MariaDB
--- PHP Version: 7.2.2
+-- Waktu pembuatan: 26 Apr 2019 pada 09.11
+-- Versi server: 10.1.38-MariaDB
+-- Versi PHP: 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,7 +25,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `p_guru`
+-- Struktur dari tabel `p_guru`
 --
 
 CREATE TABLE `p_guru` (
@@ -36,7 +36,7 @@ CREATE TABLE `p_guru` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `p_guru`
+-- Dumping data untuk tabel `p_guru`
 --
 
 INSERT INTO `p_guru` (`kd_guru`, `nip`, `nama_guru`, `gol`) VALUES
@@ -45,7 +45,7 @@ INSERT INTO `p_guru` (`kd_guru`, `nip`, `nama_guru`, `gol`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `p_kelas`
+-- Struktur dari tabel `p_kelas`
 --
 
 CREATE TABLE `p_kelas` (
@@ -55,7 +55,7 @@ CREATE TABLE `p_kelas` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `p_kelas`
+-- Dumping data untuk tabel `p_kelas`
 --
 
 INSERT INTO `p_kelas` (`id_kelas`, `nama_kelas`, `wali_kelas`) VALUES
@@ -72,7 +72,7 @@ INSERT INTO `p_kelas` (`id_kelas`, `nama_kelas`, `wali_kelas`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `p_login`
+-- Struktur dari tabel `p_login`
 --
 
 CREATE TABLE `p_login` (
@@ -85,16 +85,17 @@ CREATE TABLE `p_login` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `p_login`
+-- Dumping data untuk tabel `p_login`
 --
 
 INSERT INTO `p_login` (`id`, `name`, `username`, `password`, `log`, `waktuakses`) VALUES
-(1, 'admin', 'admin', 'admin123', 'yes', '22-04-19  08:00:02');
+(1, 'admin', 'admin', 'admin123', 'yes', '24-04-19  03:15:18'),
+(2, 'ilham fidatama', 'ilham_fidatama', 'ilham123', 'yes', '24-04-19  03:19:44');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `p_mapel`
+-- Struktur dari tabel `p_mapel`
 --
 
 CREATE TABLE `p_mapel` (
@@ -103,11 +104,10 @@ CREATE TABLE `p_mapel` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `p_mapel`
+-- Dumping data untuk tabel `p_mapel`
 --
 
 INSERT INTO `p_mapel` (`kd_mapel`, `nama_mapel`) VALUES
-('-', ''),
 ('A', 'PA & BUDI PEKERTI'),
 ('B', 'PPKn'),
 ('C', 'BHS. INDONESIA'),
@@ -134,7 +134,7 @@ INSERT INTO `p_mapel` (`kd_mapel`, `nama_mapel`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `p_nilai`
+-- Struktur dari tabel `p_nilai`
 --
 
 CREATE TABLE `p_nilai` (
@@ -144,17 +144,25 @@ CREATE TABLE `p_nilai` (
   `n_guru` varchar(20) DEFAULT NULL,
   `n_mapel` varchar(30) DEFAULT NULL,
   `n_kelas` varchar(20) DEFAULT NULL,
-  `n_tugas` int(3) DEFAULT NULL,
-  `n_kuis` int(3) DEFAULT NULL,
-  `n_uts` int(3) DEFAULT NULL,
-  `n_uas` int(3) DEFAULT NULL,
-  `n_total` varchar(3) DEFAULT NULL
+  `n_tugas` float DEFAULT NULL,
+  `n_kuis` float DEFAULT NULL,
+  `n_uts` float DEFAULT NULL,
+  `n_uas` float DEFAULT NULL,
+  `n_total` float DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `p_nilai`
+--
+
+INSERT INTO `p_nilai` (`kd_nilai`, `n_nis`, `n_nama_siswa`, `n_guru`, `n_mapel`, `n_kelas`, `n_tugas`, `n_kuis`, `n_uts`, `n_uas`, `n_total`) VALUES
+(3, '123', 'awad', '1', 'D', '2', 10, 10, 40, 40, 100),
+(4, '1231431', 'ilham', '1', 'A', '2', 8.53, 10, 40, 40, 98.53);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `p_siswa`
+-- Struktur dari tabel `p_siswa`
 --
 
 CREATE TABLE `p_siswa` (
@@ -165,48 +173,55 @@ CREATE TABLE `p_siswa` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data untuk tabel `p_siswa`
+--
+
+INSERT INTO `p_siswa` (`no_abs`, `nis`, `nama`, `s_kelas`) VALUES
+('001', '1231431', 'ilham', 'X MIPA');
+
+--
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `p_guru`
+-- Indeks untuk tabel `p_guru`
 --
 ALTER TABLE `p_guru`
   ADD PRIMARY KEY (`kd_guru`);
 
 --
--- Indexes for table `p_login`
+-- Indeks untuk tabel `p_login`
 --
 ALTER TABLE `p_login`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `p_mapel`
+-- Indeks untuk tabel `p_mapel`
 --
 ALTER TABLE `p_mapel`
   ADD PRIMARY KEY (`kd_mapel`);
 
 --
--- Indexes for table `p_nilai`
+-- Indeks untuk tabel `p_nilai`
 --
 ALTER TABLE `p_nilai`
   ADD PRIMARY KEY (`kd_nilai`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `p_login`
+-- AUTO_INCREMENT untuk tabel `p_login`
 --
 ALTER TABLE `p_login`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `p_nilai`
+-- AUTO_INCREMENT untuk tabel `p_nilai`
 --
 ALTER TABLE `p_nilai`
-  MODIFY `kd_nilai` int(3) NOT NULL AUTO_INCREMENT;
+  MODIFY `kd_nilai` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
